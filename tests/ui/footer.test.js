7978ff7de0c5114241562887621a68e1136cef8b
Pavel <pavelxragomirovdobrev@gmail.com>
2024-04-19 11:30:42 +0300

    fix url

--- tests/ui/footer.test.js
+++ tests/ui/footer.test.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check footer', async ({ page }) => {
-    await page.goto('http://localhost:8080');  
+    await page.goto('https://paffcorp.onrender.com');  
     const footer = await page.$('header');
     const text = await footer.textContent();
     expect(text).toContain('Home | View Games | Add Game | About');
